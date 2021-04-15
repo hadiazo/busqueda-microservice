6432a141ee970fb66a93029294edc13e58744de4
unknown <hadiazo@unal.edu.co>
2021-04-15 05:57:16 -0500

    Swagger

--- src/main/java/com/example/busqueda/BusquedaApplication.java
+++ src/main/java/com/example/busqueda/BusquedaApplication.java
@@ -2,6 +2,7 @@ package com.example.busqueda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.*;
 
 @SpringBootApplication
 public class BusquedaApplication {
